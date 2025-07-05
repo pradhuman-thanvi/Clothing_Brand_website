c5098016963506fea7e0226cf0823861de6745d9
Pradhuman Thanvi <154883356+pradhuman-thanvi@users.noreply.github.com>
2025-07-05 18:06:13 +0530

    Add files via upload
    updated db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "sql200.infinityfree.com";
-$user = "if0_39272233";
-$pass = "WYi8QXtJ1hDdY1";
-$dbname = "if0_39272233_thegirlyhouse"; // change to your actual DB name
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "thegirlyhouse"; // change to your actual DB name
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
